f4e23e09c3b9ede72664cc2cb304f2fb34f0cb32
Sythivo <69376875+Sythivo@users.noreply.github.com>
2025-07-15 11:03:01 -0400

    Reserve more error in api functions

--- src/VM/linit.zig
+++ src/VM/linit.zig
@@ -3,14 +3,14 @@ const std = @import("std");
 
 const lua = @import("lua.zig");
 
-pub fn Lopenlibs(L: *lua.State) void {
+pub fn Lopenlibs(L: *lua.State) !void {
     c.luaL_openlibs(@ptrCast(L));
 }
 
-pub fn Lsandbox(L: *lua.State) void {
+pub fn Lsandbox(L: *lua.State) !void {
     c.luaL_sandbox(@ptrCast(L));
 }
 
-pub fn Lsandboxthread(L: *lua.State) void {
+pub fn Lsandboxthread(L: *lua.State) !void {
     c.luaL_sandboxthread(@ptrCast(L));
 }
