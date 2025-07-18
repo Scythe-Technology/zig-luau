b5d646975efa05e6970932dc73b02ea42d237277
Sythivo <69376875+Sythivo@users.noreply.github.com>
2025-07-17 20:39:30 -0400

    Export Cst in Ast namespace

--- src/lib.zig
+++ src/lib.zig
@@ -18,6 +18,7 @@ pub const Analysis = if (build_config.buildAnalysis) struct {
 
 pub const Ast = if (build_config.buildAst) struct {
     pub const Ast = @import("Ast/Ast.zig");
+    pub const Cst = @import("Ast/Cst.zig");
     pub const Allocator = @import("Ast/Allocator.zig");
     pub const Lexer = @import("Ast/Lexer.zig");
     pub const Parser = @import("Ast/Parser.zig");
